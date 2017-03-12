7a9e4b3e500393c71f9360c2b6e12cd1ded3612e
arambabayan <arambabayan95@gmail.com>
2017-03-12 14:36:10 +0400

    Add files via upload

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> insertion_sort( std::vector<int> V )
+{
+        int n = V.size() ;
+	int key ;
+        int j ;
+        for( int i = 1 ; i < n ; ++i )
+        {
+                key = V [i] ;
+                j = i - 1 ;
+                
+		while( j >= 0 && V [j] > key )
+                {
+                        V [ j + 1 ] = V [j] ;
+                        --j;
+		}
+                
+		V [ j + 1 ] = key ;
+                
+        }
+	return V;
+
+}
+
+
+int main()
+{
+	
+//integer n shows the size of vector
+	int n ;
+	std::cin >> n ;
+	
+//vector that we want to sort
+	std::vector<int> arr( n ) ;
+	for ( int i = 0 ; i < n ; ++i )
+	{
+		std::cin >> arr [i] ;
+	} 
+//sorted vector
+	std::vector<int> sorted_arr ( insertion_sort ( arr ) ) ;
+
+//print to screen	
+	for ( std::vector<int>::iterator it = sorted_arr.begin() ; it != sorted_arr.end() ; ++it)
+	{
+		std::cout << *it << ' ' ;
+	}
+	
+	return 0;
+}
